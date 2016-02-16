21a73e3afeb00408b4f81449133133ce4f79a5d3
makitokezuka <kezuka@media-cf.co.jp>
2016-02-16 01:27:14 +0000

    flash message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,6 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only:[:edit, :update]
+  
   def index
     @message = Message.new
     #messageを全て取得する
@@ -15,6 +17,20 @@ class MessagesController < ApplicationController
       render 'index'
     end
   end
+  
+  def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      #保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      #保存に失敗した場合は編集画面にもどす
+      render 'edit'
+    end
+  end
+
   
   private
   def message_params
